e7b728a2db56f7a34b140856d44f202c0902c482
Govind Yadav <govyadav0@publicisgroupe.net>
2018-09-21 16:32:30 +0530

    add tabs

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,23 @@
 
 $(document).ready(function () {
+
+    var moveLeft = 90;
+    var moveDown = 10;
+
+    $('button#submit').hover(function (e) {
+        $('div#pop-up').show()
+        .css('top', e.pageY + moveDown)
+        .css('left', e.pageX - moveLeft)
+        .appendTo('body');
+    }, function () {
+        $('div#pop-up').hide();
+    });
+
+    $('button#submit').mousemove(function (e) {
+        $("div#pop-up").css('top', e.pageY + moveDown).css('left', e.pageX - moveLeft);
+    });
+
     $("#submit").click(function () {
-        alert("Do you want to submit?")
+        alert("Do you want to submit?") 
     });
 });
